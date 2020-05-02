609cc45c195f16fc2e9226d34043c1d793265656
michaelstresing <mstresing@gmail.com>
2020-05-02 22:34:04 +0100

    wip on styles

--- src/components/writingSection.js
+++ src/components/writingSection.js
@@ -35,18 +35,31 @@ export default () => {
     `)
 
     return (
-        <div style={{ margin: "40px 10px 40px", backgroundColor: `aqua`}}>
+        <div style={{ margin: `3%`,
+                      padding: `0.2rem`,
+                      border: `1px solid #E75954`,
+                      height: `30vh`,
+                      width: `94%`,
+                      display: `flex`,
+                      overflowX: `scroll`}}>
             {data.allMdx.edges.map(({ node }) => {
             const title = node.frontmatter.title || node.fields.slug
             return (
-                <div key={node.fields.slug} style={{ margin: "20px 20px", backgroundColor: `green`}}>
+                <div key={node.fields.slug} style={{ display: `block`,
+                                                     margin: `20px 20px`,
+                                                     padding: `1rem`, 
+                                                     border: `1px solid green`,
+                                                     minHeight: `20rem`,
+                                                     minWidth: `20rem`}}>
                 <h3
                     style={{
                     marginBottom: rhythm(1 / 4),
                     }}
                 >
                     <Link
-                    style={{ boxShadow: `none`, color: `red` }}
+                    style={{ boxShadow: `none`,
+                             color: `black`,
+                             margin: `auto` }}
                     to={`blog${node.fields.slug}`}
                     state={{
                         modal: true
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,14 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import Img from "../components/image"
 import SEO from "../components/seo"
 import Writing from "../components/writingSection"
-import henry_mov from "../../content/assets/chatrooming.mp4"
 
 
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home"/>
-    <div style={{ maxWidth: `1200px`, marginBottom: `1.45rem`, display: `flex`, flexDirection: `row`   }}>
-      <Img  />
-    </div> 
+    <Writing />
     <Writing />
   </Layout>
 )
